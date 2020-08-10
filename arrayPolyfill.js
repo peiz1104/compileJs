ce6f76189248a86e0009c0881d97b16abdbb7278
pmj <peiz1104@users.noreply.github.com>
2020-08-10 15:09:51 +0800

    Update arrayPolyfill.js

--- arrayPolyfill.js
+++ arrayPolyfill.js
@@ -121,31 +121,36 @@ function shouldUseNative() {
 	}
 }
 // 9、支持Object.assign的pollyfill
+function toObject(val) {
+  if (val === null || val === undefined) {
+	throw new TypeError('Object.assign cannot be called with null or undefined');
+  }
+  return Object(val);
+}
 function (target, source) {
  var getOwnPropertySymbols = Object.getOwnPropertySymbols;
  var hasOwnProperty = Object.prototype.hasOwnProperty;
  var propIsEnumerable = Object.prototype.propertyIsEnumerable;
-	var from;
-	var to = toObject(target);
-	var symbols;
+ var from;
+ var to = toObject(target);
+ var symbols;
 
-	for (var s = 1; s < arguments.length; s++) {
-		from = Object(arguments[s]);
+ for (var s = 1; s < arguments.length; s++) {
+	from = Object(arguments[s]);
 
-		for (var key in from) {
-			if (hasOwnProperty.call(from, key)) {
-				to[key] = from[key];
-			}
-		}
-
-		if (getOwnPropertySymbols) {
-			symbols = getOwnPropertySymbols(from);
-			for (var i = 0; i < symbols.length; i++) {
-				if (propIsEnumerable.call(from, symbols[i])) {
-					to[symbols[i]] = from[symbols[i]];
-				}
-			}
-		}
+	 for (var key in from) {
+	   if (hasOwnProperty.call(from, key)) {
+	     to[key] = from[key];
+	   }
+	 }
+	if (getOwnPropertySymbols) {
+	  symbols = getOwnPropertySymbols(from);
+            for (var i = 0; i < symbols.length; i++) {
+	      if (propIsEnumerable.call(from, symbols[i])) {
+		   to[symbols[i]] = from[symbols[i]];
+		 }
+	    }
 	}
-	return to;
+  }
+  return to;
 }
