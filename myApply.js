2879f65402f50e9cfafe447950abcc1ee52f41f2
pmj <peiz1104@users.noreply.github.com>
2020-08-27 16:01:43 +0800

    apply
    apply call

---
+++ myApply.js
@@ -0,0 +1,27 @@
+function mySymbol(obj){
+ let symbolK = Math.random().toString(36).slice(-10)
+ if(obj.hasOwnProperty(symbolK)){
+   mySymbol(obj)
+ }
+ return symbolK
+}
+Function.prototype.myApply = function(context){
+  if(typeof this !=='function') throw new TypeError('context must be function')
+  context = context || window
+  if(arguments[1]&&typeof arguments[1]!=='object') throw new TypeError('')
+  var arg = arguments[1]? [...arguments].slice(1):[]
+  var fn = mySymbol(context)
+  var context[fn]= this
+  context[fn](...arg)
+  delete context[fn]
+}
+
+Function.prototype.myCall = function(context){
+ if(typeof this !=='function') throw new TypeError('context must be function')
+ context = context || window
+ var arg = [...arguments].slice(1)
+ var fn = mySymbol(context)
+ var context[fn] = this
+ context[fn](...arg)
+ delete context[fn]
+}
