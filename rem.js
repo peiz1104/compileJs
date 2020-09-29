6a0140c81765924ee923794c95452cba7d9b5668
pmj <peiz1104@users.noreply.github.com>
2020-09-29 14:34:35 +0800

    Update rem.js

--- rem.js
+++ rem.js
@@ -20,3 +20,22 @@
   x.addEventListener("resize", function () { w() });
   w()
 }(window);
+
+// js计算
+(function(doc,win){
+ var docEl = doc.documentElement,
+     resizeEvt = 'orientationchange' in win ? 'orientationchange':'resize',
+     recalc = funtion(){
+      var clientWidth = win.clientWidth
+      if(!clientWidth) return
+       if(clientWidth>=750) {
+        docEl.style.fontSize='100px'
+       }else{
+        docEl.style.fontSize = 100*(750/clientWidth)+'px'
+       }
+     }
+  if(!doc.addEventListener) return
+  win.addEventListener(resizeEvt,recalc,false)
+  doc.addEventListener('DOMContentLoaded',recalc,false)
+})(document,window)
+
